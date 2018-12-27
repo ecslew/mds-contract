ceb34ee956c0987b46c5784924134f084e2e3512
rayshitou <dlleig@gmail.com>
2018-12-27 18:56:14 +0800

    first commit

---
+++ medishares.cpp
@@ -0,0 +1,144 @@
+#include <medishares.hpp>
+#include <eosiolib/time.hpp>
+
+using namespace eosio;
+
+uint64_t year_second = 365*24*3600;
+
+void medishares::add(const account_name initiator, const std::string name, const checksum256 item_digest, const account_name receiver, const extended_asset& min_fund, const extended_asset& max_fund, const extended_asset& target_fund, const uint64_t deadline)
+{
+	eosio_assert( name.size() <= 256, "the name of item has more than 256 bytes");
+	eosio_assert( is_account(receiver), "receiver account does not exist");
+	eosio_assert( min_fund.is_valid() && max_fund.is_valid() && target_fund.is_valid(), "invalid fund" );
+	eosio_assert( min_fund.amount > 0 && min_fund.amount <= max_fund.amount && max_fund.amount <= target_fund.amount, "invalid fund quantity" );
+	eosio_assert( min_fund.contract == max_fund.contract && max_fund.contract == target_fund.contract, "inconsistent asset");
+	eosio_assert( is_account(min_fund.contract), "invalid asset");
+	eosio_assert( deadline > now() && deadline - now() < year_second, "invalid deadline");
+
+	require_auth(initiator);
+
+	auto glb = global.begin();
+    if(glb == global.end()){
+        glb = global.emplace(_self, [&](auto& gl){
+            gl.total_item = 1;
+            gl.completed_item = 0;
+            gl.ongoing_item = 0;
+			gl.auditor = _self;
+        }); 
+    }else{
+        global.modify(glb, 0, [&](auto& gl){
+        	gl.total_item = gl.total_item + 1;
+		}); 
+    }
+	
+	items_table items(_self, initiator);
+	auto ite = items.begin();
+	while(ite != items.end()){
+		if(ite->name == name)
+			eosio_assert(false, "the item name aready exist");
+		ite++;
+	}
+	
+    items.emplace(initiator, [&](auto& it){
+        it.id = glb->total_item;
+        it.name = name;
+		it.item_digest = item_digest;
+        it.initiator = initiator;
+        it.receiver = receiver;
+        it.min_fund = min_fund;
+		it.max_fund = max_fund;
+		it.target_fund = target_fund;
+        it.start = now();
+		it.deadline = deadline;
+		it.status = 0;
+    }); 
+
+}
+    
+void medishares::modify(const account_name initiator, const uint64_t id, const std::string name, const checksum256 item_digest, const account_name receiver, const extended_asset& min_fund, const extended_asset& max_fund, const extended_asset& target_fund, const uint64_t deadline)
+{
+    eosio_assert( name.size() <= 256, "the name of item has more than 256 bytes");
+    eosio_assert( is_account(receiver), "receiver account does not exist");    
+	eosio_assert( min_fund.is_valid() && max_fund.is_valid() && target_fund.is_valid(), "invalid fund" );    
+	eosio_assert( min_fund.amount > 0 && min_fund.amount <= max_fund.amount && max_fund.amount <= target_fund.amount, "invalid fund quantity" );
+    eosio_assert( min_fund.contract == max_fund.contract && max_fund.contract == target_fund.contract, "inconsistent asset");
+    eosio_assert( is_account(min_fund.contract), "invalid asset");
+    eosio_assert( deadline > now() && deadline - now() < year_second, "invalid deadline");
+
+	items_table items(_self, initiator);
+	auto ite = items.begin();
+    while(ite != items.end() && ite->id != id)
+        ite++;
+    eosio_assert(ite != items.end(), "the item not exist");
+	eosio_assert(ite->status < 2, "item can not be modified after audited");
+
+	require_auth(ite->initiator);
+	
+    items.modify(ite, 0, [&](auto& it){
+        it.name = name;
+        it.item_digest = item_digest;
+        it.receiver = receiver;
+        it.min_fund = min_fund;
+        it.max_fund = max_fund;
+        it.target_fund = target_fund;
+        it.deadline = deadline;
+		it.status = 0;
+    });
+}
+
+void medishares::erase(const account_name initiator, const std::string name)
+{
+	items_table items(_self, initiator);
+    auto ite = items.begin();
+    while(ite != items.end() && ite->name != name)
+        ite++;
+    eosio_assert(ite != items.end(), "the item not exist");
+    eosio_assert(ite->status < 2, "item cannot be deleted after audited");
+
+	require_auth(ite->initiator);    
+    items.erase(ite);
+}
+
+void medishares::audit(const account_name initiator, const std::string name, const uint8_t flag)
+{
+	items_table items(_self, initiator);
+    auto ite = items.begin();
+    while(ite != items.end() && ite->name != name)
+        ite++;
+    eosio_assert(ite != items.end(), "the item not exist");
+    eosio_assert(ite->status < 2, "item has been audited aready");
+    eosio_assert(ite->deadline > now(), "item expired");
+
+	auto glb = global.begin();
+	eosio_assert(glb != global.end(), "the global table does not exist");
+    require_auth(glb->auditor);
+
+	if(flag == 2){//审核通过
+		items.modify(ite, 0, [&](auto& it){
+			it.start = now();
+			it.status = 2;
+		});
+
+        global.modify(glb, 0, [&](auto& gl){
+            gl.ongoing_item = gl.ongoing_item + 1;
+        });
+	}else if(flag == 1){//审核不通过，待修改
+		items.modify(ite, 0, [&](auto& it){
+            it.status = 1;
+        });
+	}else if(flag == 0){//不合规，删除
+		items.erase(ite);
+	}
+}
+
+void medishares::setauditor(const account_name auditor)
+{
+	eosio_assert( is_account(auditor), "auditor account does not exist");
+	auto glb = global.begin();
+    eosio_assert(glb != global.end(), "the global table does not exist");
+    require_auth(_self);
+
+	global.modify(glb, 0, [&](auto& gl){
+        gl.auditor = auditor;
+    });
+}
---
+++ medishares.hpp
@@ -0,0 +1,68 @@
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/transaction.hpp>
+#include <eosiolib/asset.hpp>
+#include <string>
+
+using namespace eosio;
+using std::string;
+using namespace std;
+
+class medishares: public eosio::contract{
+  public:
+    medishares(account_name self):
+    contract(self),
+	global(_self, _self){}
+
+    ///@abi action
+	void add(const account_name initiator, const std::string name, const checksum256 item_digest, const account_name receiver, const extended_asset& min_fund, const extended_asset& max_fund, const extended_asset& target_fund, const uint64_t deadline);
+
+    ///@abi action
+	void modify(const account_name initiator, const uint64_t id, const std::string name, const checksum256 item_digest, const account_name receiver, const extended_asset& min_fund, const extended_asset& max_fund, const extended_asset& target_fund, const uint64_t deadline);
+
+    ///@abi action
+	void erase(const account_name initiator, const std::string name);
+	
+	///@abi action
+	void audit(const account_name initiator, const std::string name, const uint8_t flag);
+	
+	///@abi action
+    void setauditor(const account_name auditor);
+	
+  private:
+	///@abi table
+	struct global
+	{
+		uint64_t total_item;
+		uint64_t completed_item;
+		uint64_t ongoing_item;
+		account_name auditor;//项目审核员，可以为多签账户
+	
+		auto primary_key()const{return 0;}
+		EOSLIB_SERIALIZE(global, (total_item)(completed_item)(ongoing_item)(auditor))
+	};
+  	eosio::multi_index<N(global), global> global;
+
+	///@abi table
+    struct item
+    {
+		uint64_t		id=1;
+		std::string 	name;
+		checksum256		item_digest;
+		account_name	initiator;
+		account_name	receiver;
+        extended_asset  min_fund;
+        extended_asset  max_fund;
+		extended_asset  target_fund;
+		time			start;
+		time 			deadline;
+		uint8_t			status; //0：待审核，1：待修改，2：筹款中，3：筹款结束
+		
+        auto primary_key()const{return id;}
+        EOSLIB_SERIALIZE(item, (id)(name)(item_digest)(initiator)(receiver)(min_fund)(max_fund)(target_fund)(start)(deadline)(status))
+    };
+    typedef eosio::multi_index<N(item), item> items_table;
+
+};
+
+EOSIO_ABI(medishares, (add)(modify)(erase)(audit)(setauditor))
+
